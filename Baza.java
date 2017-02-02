8c354a922302506ccf2758aaa882052ce7afdedb
CristianDAM <gutierrezperezcristian@gmail.com>
2017-02-02 10:46:42 +0100

    Primer apartado

---
+++ Baza.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Baza here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Baza
+{
+   private int jugadoresEnLaBaza;
+   
+    /**
+     * Constructor for objects of class Baza
+     */
+    public Baza(int numJugadoresTiranCarta, int paloQuePinta)
+    {
+        jugadoresEnLaBaza = numJugadoresTiranCarta;
+       
+    }
+
+  
+}
